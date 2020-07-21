93c2e4a0ce23dc555a55c1f89740df538e575fb6
Kajal Soni <kaajalsoni121@gmail.com>
2020-07-21 23:11:05 +0530

    Dynamic lookups,404

--- tweets/views.py
+++ tweets/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Tweet
 # Create your views here.
 def home_view(request, *args, **kwargs):
+    #print(args,kwargs)
     return HttpResponse("<h1>Hello World</h1>")
 
 def tweet_detail_view(request, tweet_id,*args, **kwargs):
-    return HttpResponse(f"<h1>Hello {tweet_id}</h1>")
+    obj=Tweet.objects.get(id=tweet_id)
+    return HttpResponse(f"<h1>Hello {tweet_id}-{obj.content}</h1>")
