93f9f3fb0e8a08860b186ef7d50139f83e45d8a5
Kajal Soni <kaajalsoni121@gmail.com>
2020-07-23 01:46:17 +0530

    22-forms

--- tweets/views.py
+++ tweets/views.py
@@ -2,6 +2,7 @@ import random
 from django.shortcuts import render
 from django.http import HttpResponse,Http404,JsonResponse
 from .models import Tweet
+from .forms import TweetForm
 # Create your views here.
 def home_view(request, *args, **kwargs):
     return render(request,"pages/home.html", context={}, status=200)
@@ -37,3 +38,12 @@ def tweet_list_view(request, *args, **kwargs):
         "response":tweets_list
         }
     return JsonResponse(data)
+
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        # do other form related logic
+        obj.save()
+        form = TweetForm()
+    return render(request, 'components/form.html', context={"form": form})
